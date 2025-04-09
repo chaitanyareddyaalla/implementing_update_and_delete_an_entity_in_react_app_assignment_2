90d2efa9573fea9dee05536e7d6808fd53e20dd3
Aalla Chaitanya Reddy <daxingamerz358@gmail.com>
2025-04-09 16:13:34 +0530

    done

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,29 @@
-const Item = ({ item }) => {
+import UpdateItem from "./components/UpdateItem";
+import React, { useEffect, useState } from "react";
 
-    // Render a single item
-    // Add a Delete and Edit button
-    return null;
-};
+// use the following link to get the data
+// /doors will give you all the doors, to get a specific door use /doors/1.
+const API_URI = http://${import.meta.env.VITE_API_URI}/doors;
 
-export default Item;
+function App() {
+  // Get the existing item from the server
+  const [item, setItem] = useState(null);
+
+  // pass the item to UpdateItem as a prop
+  useEffect(() => {
+    fetch(${API_URI}/1)
+      .then((res)=> res.json())
+      .then((data) => setItem(data))
+      .catch((err) => console.error("Failed to fetch item", err));
+  }, []);
+
+  return (
+  <div>
+    <h1>Update Door</h1>
+    {item ? <UpdateItem item={item}/> : <p>Loading...</p>}
+  </div>
+
+  );
+}
+
+export default App;
